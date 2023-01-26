6ac882719a975810b28caba642c0cf408603aa2d
David <hexadecagon@gmail.com>
2023-01-26 17:28:59 -0600

    renamed column "ITEM.1" to "QTY"

--- pandas_python.py
+++ pandas_python.py
@@ -17,6 +17,9 @@ df = pd.read_excel(excel_file, sheet_name=0, header=1)
 #get rid of the top line of garbage
 df = df[1:]
 
+#rename column "ITEM.1" to "QTY"
+df.rename(columns = {'ITEM.1':'QTY'}, inplace=True)
+
 
 #####Angle order#####
 
@@ -119,9 +122,9 @@ dfNutsAndBolts = dfNutsAndBolts.drop('WEIGHT', axis=1)
 #filter for shop bolts and field bolts. filter is whether sheet name contains an E
 dfShopBolts = dfNutsAndBolts[~dfNutsAndBolts['DRAWING'].str.contains("E", na=False, case=False)]
 #get a sum of bolts by type and station
-dfShopBolts = dfShopBolts.groupby(['MATERIAL DESCRIPTION','GRADE','DRAWING','STRUCTURES','ITEM.1']).sum(numeric_only=True).reset_index()
+dfShopBolts = dfShopBolts.groupby(['MATERIAL DESCRIPTION','GRADE','DRAWING','STRUCTURES','QTY']).sum(numeric_only=True).reset_index()
 #add 8% or +5 to shop bolts, whichever is more
-dfShopBolts['ORDER'] = dfShopBolts['ITEM.1'].apply(lambda row:(row*1.08) if row>62 else (row+5))
+dfShopBolts['ORDER'] = dfShopBolts['QTY'].apply(lambda row:(row*1.08) if row>62 else (row+5))
 #round up
 dfShopBolts['ORDER'] = dfShopBolts['ORDER'].apply(np.ceil)
 #save to separate excel file
@@ -130,9 +133,9 @@ dfShopBolts.to_excel("ShopNuts&Bolts.xlsx", sheet_name="Sheet 1")
 dfFieldBolts = dfNutsAndBolts[dfNutsAndBolts['DRAWING'].str.contains("E", na=False, case=False)]
 #add 2 to each bolt count
 #need to fix this so it doesn't give me warnings every time
-dfFieldBolts['ORDER'] = dfFieldBolts.apply(lambda row:(row['ITEM.1'] + 2),axis=1)
+dfFieldBolts['ORDER'] = dfFieldBolts.apply(lambda row:(row['QTY'] + 2),axis=1)
 #get a sum of bolts by type and station
-dfFieldBolts = dfFieldBolts.groupby(['MATERIAL DESCRIPTION','GRADE','DRAWING','STRUCTURES', 'ITEM.1', 'ORDER']).sum(numeric_only=True).reset_index()
+dfFieldBolts = dfFieldBolts.groupby(['MATERIAL DESCRIPTION','GRADE','DRAWING','STRUCTURES', 'QTY', 'ORDER']).sum(numeric_only=True).reset_index()
 #save to new excel file
 dfFieldBolts.to_excel("FieldNuts&Bolts.xlsx", sheet_name="Sheet 1")
 
