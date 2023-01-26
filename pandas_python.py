4b80b7e65e6fdb753b2f9aa9bf244f6a173e3190
David <hexadecagon@gmail.com>
2023-01-26 12:52:36 -0600

    fixed error in bolt count

--- pandas_python.py
+++ pandas_python.py
@@ -104,33 +104,35 @@ dfMisc.to_excel("MiscMaterial.xlsx", sheet_name="Sheet 1")
 
 #filter out everyhing but nuts, bolts, and washers only
 dfNutsAndBolts = df[df['PART DESCRIPTION'].str.contains("nut*|bolt*|washer*", na=False, case=False)]
+dfNutsAndBolts.to_excel("DEBUGNuts&Bolts.xlsx", sheet_name="Sheet 1")
 #sort by column MATERIAL DESCRIPTION
 dfNutsAndBolts = dfNutsAndBolts.sort_values('MATERIAL DESCRIPTION')
 #explodes entries with multiple stations to one line per station
-dfNutsAndBolts = dfNutsAndBolts.assign(STRUCTURES=dfNutsAndBolts['STRUCTURES'].str.strip().str.split("|")).explode('STRUCTURES').reset_index(drop=True)
+dfNutsAndBoltsG = dfNutsAndBolts.assign(STRUCTURES=dfNutsAndBolts['STRUCTURES'].str.strip().str.split("|")).explode('STRUCTURES').reset_index(drop=True)
 #deleting assy and total columns to avoid confusion now structures are one pre line
 dfNutsAndBolts = dfNutsAndBolts.drop('ASSY.', axis=1)
 dfNutsAndBolts = dfNutsAndBolts.drop('TOTAL', axis=1)
 #deleting columns that don't help ordering nust and bolts
+dfNutsAndBolts = dfNutsAndBolts.drop('ITEM', axis=1)
 dfNutsAndBolts = dfNutsAndBolts.drop('REV', axis=1)
 dfNutsAndBolts = dfNutsAndBolts.drop('WEIGHT', axis=1)
 #filter for shop bolts and field bolts. filter is whether sheet name contains an E
 dfShopBolts = dfNutsAndBolts[~dfNutsAndBolts['DRAWING'].str.contains("E", na=False, case=False)]
 #get a sum of bolts by type and station
-dfShopBolts = dfShopBolts.groupby(['MATERIAL DESCRIPTION','STRUCTURES']).sum(numeric_only=True)
+dfShopBolts = dfShopBolts.groupby(['MATERIAL DESCRIPTION','STRUCTURES','ITEM.1']).sum(numeric_only=True).reset_index()
 #add 8% or +5 to shop bolts, whichever is more
-dfShopBolts['ORDER'] = dfShopBolts["ITEM"].apply(lambda row:(row*1.08) if row>62 else (row+5))
+dfShopBolts['ORDER'] = dfShopBolts['ITEM.1'].apply(lambda row:(row*1.08) if row>62 else (row+5))
 #round up
-dfShopBolts['ORDER'] = dfShopBolts["ORDER"].apply(np.ceil)
+dfShopBolts['ORDER'] = dfShopBolts['ORDER'].apply(np.ceil)
 #save to separate excel file
 dfShopBolts.to_excel("ShopNuts&Bolts.xlsx", sheet_name="Sheet 1")
 #filter for shop bolts and field bolts. filter is whether sheet name contains an E
 dfFieldBolts = dfNutsAndBolts[dfNutsAndBolts['DRAWING'].str.contains("E", na=False, case=False)]
 #add 2 to each bolt count
 #need to fix this so it doesn't give me warnings every time
-dfFieldBolts['ORDER'] = dfFieldBolts.apply(lambda row:(row.loc['ITEM'] + 2),axis=1)
+dfFieldBolts['ORDER'] = dfFieldBolts.apply(lambda row:(row['ITEM.1'] + 2),axis=1)
 #get a sum of bolts by type and station
-dfFieldBolts = dfFieldBolts.groupby(['MATERIAL DESCRIPTION','STRUCTURES']).sum(numeric_only=True)
+dfFieldBolts = dfFieldBolts.groupby(['MATERIAL DESCRIPTION','STRUCTURES', 'ITEM.1', 'ORDER']).sum(numeric_only=True).reset_index()
 #save to new excel file
 dfFieldBolts.to_excel("FieldNuts&Bolts.xlsx", sheet_name="Sheet 1")
 
